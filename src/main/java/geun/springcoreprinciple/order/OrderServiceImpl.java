728eb6262d23fad5e579a918456fdff284b6a2a9
Geunwoo Lee <player_geun@naver.com>
2022-03-22 13:42:14 +0900

    Auto DI #3 - lombok
    최근에는 생성자를 딱 1개두고, @Autowired를 생략하는 방업을 주로 사용 
    여기에 Lombok라이브러리의 @RequiredArgsConstructor 함께 사용하면 기능은 다제공하면서, 코드는 깔끔하게 사용가능

--- src/main/java/geun/springcoreprinciple/order/OrderServiceImpl.java
+++ src/main/java/geun/springcoreprinciple/order/OrderServiceImpl.java
@@ -5,10 +5,12 @@ import geun.springcoreprinciple.discount.FixDiscountPolicy;
 import geun.springcoreprinciple.member.Member;
 import geun.springcoreprinciple.member.MemberRepositroy;
 import geun.springcoreprinciple.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 //    @Autowired        필드 주입 DI 프레임워크가 없으면 아무것도 못함, 테스트 하기 힘듦
@@ -17,10 +19,11 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
 //    @Autowired        생성자가 딱 1개만 있으면 @Autowired 생략해도 자동 주입, 물론 스프링 빈에만 해당
-    public OrderServiceImpl(MemberRepositroy memberRepositroy, DiscountPolicy discountPolicy) {
-        this.memberRepositroy = memberRepositroy;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepositroy memberRepositroy, DiscountPolicy discountPolicy) {
+//        this.memberRepositroy = memberRepositroy;
+//        this.discountPolicy = discountPolicy;
+//    }
+    // lombok 덕분에 생략 가능
 
 //    @Autowired            수정자 주입(setter 주입) : 주입할 대상이 없어도 동작하게 하려면 Autowired(required = false)
 //    public void setMemberRepositroy(MemberRepositroy memberRepositroy) {
